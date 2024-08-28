4be510e48ad97953fb4bcdcbd39209e06449d784
santhosh830 <santhosh65789@gmail.com>
2024-08-28 12:34:06 +0530

    'done'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white overflow-x-hidden'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-20'>
-
                As a recent graduate with a Bachelor's degree in Computer Science (BSc) and over one and a half years of experience in a non-voice process at Sutherland Global Services, I have developed a strong foundation in problem-solving and customer service. My passion for technology and web development led me to pursue a comprehensive Python full stack development course, equipping me with the skills to build dynamic and responsive web applications.
 
                Throughout my academic and professional journey, I have honed my abilities in both front-end and back-end development, gaining proficiency in Python, JavaScript, HTML, CSS, and various frameworks. My hands-on experience with a demo project has further solidified my understanding of the full stack development process, from designing user interfaces to managing databases.
@@ -20,4 +19,4 @@ const About = () => {
   )
 }
 
-export default About
+export default About;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,38 +10,38 @@ const Portfolio = () => {
         {
             id: 1,
             src: chatroom,
-            demoLink: 'http://13.60.95.206:8000/',  // Replace with your actual demo URL
-            codeLink: 'https://github.com/santhosh830/Teamchatroom', // Replace with your actual code URL
+            demoLink: 'http://13.60.95.206:8000/',  
+            codeLink: 'https://github.com/santhosh830/Teamchatroom',
         },
         {
             id: 2,
             src: cloudservices,
-            demoLink: 'https://webcloudservices.netlify.app/',  // Replace with your actual demo URL
-            codeLink: 'https://github.com/santhosh830/yt-cloud-web-services-website', // Replace with your actual code URL
+            demoLink: 'https://webcloudservices.netlify.app/',
+            codeLink: 'https://github.com/santhosh830/yt-cloud-web-services-website',
         },
         {
             id: 3,
             src: djangovideo,
-            demoLink: 'http://51.20.67.64:8000/',  // Replace with your actual demo URL
-            codeLink: 'https://github.com/santhosh830/Videochatroom', // Replace with your actual code URL
+            demoLink: 'http://51.20.67.64:8000/',
+            codeLink: 'https://github.com/santhosh830/Videochatroom',
         },
         {
             id: 4,
             src: ecommerce,
-            demoLink: 'https://e-commercecloths.netlify.app/',  // Replace with your actual demo URL
-            codeLink: 'https://github.com/santhosh830/E-Commerce', // Replace with your actual code URL
+            demoLink: 'https://e-commercecloths.netlify.app/',
+            codeLink: 'https://github.com/santhosh830/E-Commerce',
         },
         {
             id: 5,
             src: weather,
-            demoLink: 'https://weatherupdateapplication.netlify.app/',  // Replace with your actual demo URL
-            codeLink: 'https://github.com/santhosh830/Weatherapplication', // Replace with your actual code URL
+            demoLink: 'https://weatherupdateapplication.netlify.app/',
+            codeLink: 'https://github.com/santhosh830/Weatherapplication',
         },
     ];
 
     return (
-        <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
+        <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen overflow-x-hidden'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                     <p className='py-6'>Check out some of my demo work right here</p>
@@ -49,7 +49,7 @@ const Portfolio = () => {
 
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
                     {portfolios.map(({ id, src, demoLink, codeLink }) => (
-                        <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                        <div key={id} className='shadow-md shadow-gray-600 rounded-lg overflow-hidden'>
                             <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
                             <div className='flex items-center justify-center'>
                                 <a href={demoLink} target="_blank" rel="noopener noreferrer" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center'>Demo</a>
